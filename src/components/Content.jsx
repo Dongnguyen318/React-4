b3fdf5890cfd9f316f00e4728d5711ab112c74e4
quydong <quydong318@gmail.com>
2023-11-25 21:59:01 +0700

    nop bai tap

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,15 +1,19 @@
+/** @format */
+
 import React, { useLayoutEffect } from "react";
 import { SurpriseNumber } from "../App";
 import { useContext } from "react";
 import { MAX_TIME } from "./config";
 
 export default function Content() {
-  const { range,turn } = useContext(SurpriseNumber);
+  const { range, turn } = useContext(SurpriseNumber);
   return (
     <>
-      <h2 className="title">Chào mừng bạn đến với trò chơi đoán số</h2>
-      <h2 >số lần còn lại: {turn}/ {MAX_TIME(range)} </h2>
-      <h2>Bạn Cần tìm kiếm một số từ 1 đến {range}</h2>
+      <h2 className="title">Chơi đoán số không bạn ?</h2>
+      <h2>
+        Số lần còn lại: {turn}/ {MAX_TIME(range)}{" "}
+      </h2>
+      <h2>đơn giản bạn chỉ cần đoán đúng 1 số từ {range}</h2>
     </>
   );
 }
